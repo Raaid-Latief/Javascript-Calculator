13bc8a7b599aa9ec965d309b8842144aca1b8153
Raaid-Latief <raaidlatiefgbr@gmail.com>
2022-06-08 14:52:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+
+
+let sum = " ";
+
+function buildSum(char) {
+    sum += char;
+    document.getElementById("display").innerHTML = sum;
+}
+
+function calc() {
+    sum = eval(sum);
+    document.getElementById("display").innerHTML = null;
+    document.getElementById("display").innerHTML = sum;
+}
+
+function clrSum() {
+    document.getElementById("display").innerHTML = null;
+    sum = " ";
+}
+
+function changered() {
+    document.getElementById("display").style.color = "red";
+}
+
+function changered() {
+    document.getElementById("display").style.color = "red";
+}
+
+function changered() {
+    document.getElementById("display").style.color = "red";
+}
+
+
+
+  
+  
+  
+  
+  
+  
